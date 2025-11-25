9df0bf0c6c41919f7adda394b65ef6f908465159
trafferteam-13 <trafferteam@gmail.com>
2025-11-26 00:46:14 +0300

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -6,7 +6,7 @@ from aiogram.filters import Command
 from aiogram.types import BufferedInputFile
 from aiogram import F
 
-API_TOKEN = "8205771927:AAG14vgGguDnDpWw_0VvTwvsaDam8wc5jpc"
+API_TOKEN = "8205771927:AAE6kjctmtqNlT8ux7n-cHLO4wtkeVemB9A"
 KIE_API_KEY = "39c5516044ea71607108799a405f971b"
 
 bot = Bot(token=API_TOKEN)
